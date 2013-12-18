5cb1bf623f3af357f6b98615fb7720ccf14de4d6
Maria Shaldibina <mariash@pivotallabs.com>
2013-12-18 11:02:42 -0800

    Spec collaboration to ensure runner is always injected correctly
    Signed-off-by: Abhijit Hiremagalur <abhi@pivotallabs.com>

--- spec/cf_deploy_spec.rb
+++ spec/cf_deploy_spec.rb
@@ -9,42 +9,87 @@ module CfDeployer
   describe CfDeploy do
     describe "#deploy" do
       subject(:cf_deploy) do
-        described_class.new(
-          double(:options,
-            rebase: rebase,
-
-            repos_path: nil,
-            deployments_repo: nil,
-            release_repo: nil,
-            release_name: 'release-name',
-            release_ref: nil,
-            deployment_name: 'deployment-name',
-            interactive: false,
-            infrastructure: infrastructure,
-            final_release: is_final_release,
-            install_tokens: false,
-            promote_branch: nil,
-            dirty: false,
-          ).as_null_object,
-          logger
-        )
+        described_class.new(options, logger)
       end
 
       let(:null_object) { double(:null_object).as_null_object }
+
+      let(:release_names) do
+        release_names = double(:release_names)
+        release_names.stub(:zip).and_yield('fake-name', 'fake-release_repo', 'fake-ref')
+        release_names
+      end
+
       let(:logger) { null_object }
-      let(:runner) { null_object }
+      let(:runner) { double(CommandRunner) }
+      let(:deployment ) { double(Deployment, :bosh_environment => "bosh-environment") }
+      let(:manifest_generator) { double(ReleaseManifestGenerator) }
+      let(:release_repo) { double(ReleaseRepo, :sync! => nil) }
       let(:is_final_release) { false }
       let(:rebase) { false }
       let(:infrastructure) { "aws" }
+      let(:options) do
+        double(:options,
+               rebase: rebase,
+               repos_path: '/path/to/repos',
+               deployments_repo: 'fake-deployments_repo',
+               release_repo: nil,
+               release_names: release_names,
+               release_ref: nil,
+               deployment_name: 'deployment-name',
+               interactive: false,
+               infrastructure: infrastructure,
+               final_release: is_final_release,
+               install_tokens: true,
+               promote_branch: nil,
+               dirty: false,
+        ).as_null_object
+      end
 
       before do
         allow(Repo).to receive(:new).and_return(null_object)
-        allow(ReleaseRepo).to receive(:new).and_return(null_object)
-        allow(Deployment).to receive(:new).and_return(null_object)
+        allow(ReleaseRepo).to receive(:new).and_return(release_repo)
+        allow(Deployment).to receive(:new).and_return(deployment)
         allow(Bosh).to receive(:new).and_return(null_object)
-        allow(ReleaseManifestGenerator).to receive(:new).and_return(null_object)
+        allow(ReleaseManifestGenerator).to receive(:new).and_return(manifest_generator)
         allow(DevDeploymentStrategy).to receive(:new).and_return(null_object)
         allow(DatadogEmitter).to receive(:new).and_return(null_object)
+        allow(TokenInstaller).to receive(:new).and_return(null_object)
+      end
+
+      context "instantiation of collaborators" do
+        specify Repo do
+          cf_deploy.deploy(runner)
+
+          expect(Repo).to have_received(:new).with(logger, runner, "/path/to/repos", "fake-deployments_repo", "origin/master")
+        end
+
+        specify ReleaseRepo do
+          cf_deploy.deploy(runner)
+
+          expect(ReleaseRepo).to have_received(:new).with(logger, runner, "/path/to/repos", "fake-release_repo", "fake-ref")
+        end
+
+        specify Bosh do
+          cf_deploy.deploy(runner)
+
+          expect(Bosh).to have_received(:new)
+                          .with(logger, runner, "bosh-environment", interactive: false, rebase: rebase, dirty: false)
+        end
+
+        specify ReleaseManifestGenerator do
+          cf_deploy.deploy(runner)
+
+          expect(ReleaseManifestGenerator).to have_received(:new)
+                                              .with(runner, release_repo, infrastructure, "new_deployment.yml")
+        end
+
+        specify TokenInstaller do
+          cf_deploy.deploy(runner)
+
+          expect(TokenInstaller).to have_received(:new)
+                                              .with(logger, manifest_generator, runner)
+        end
       end
 
       context "when the rebase option is false" do
