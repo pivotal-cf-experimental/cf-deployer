6344f8127aa2f132e8b82db8fb20691ee09d2cf8
Abhijit Hiremagalur <abhi@pivotallabs.com>
2014-02-07 14:02:26 -0800

    Remove code that bundle exec handles
    Signed-off-by: Tammer Saleh <tammer@pivotallabs.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,3 @@
-require 'bundler'
-
-$:.unshift(File.expand_path("../../lib", __FILE__))
-
-Bundler.require
-
 Dir.glob(File.expand_path("../support/*.rb", __FILE__)).each do |support|
   require support
 end
