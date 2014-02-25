146e0c8fead0fcbdf78554ee8051f0210ea012b6
Mark Rushakoff <mrushakoff@pivotallabs.com>
2014-02-25 15:04:55 -0800

    Normalize style
    Signed-off-by: Aram Price <aprice@pivotallabs.com>

--- lib/cf_deployer/cf_deploy.rb
+++ lib/cf_deployer/cf_deploy.rb
@@ -23,29 +23,26 @@ module CfDeployer
 
     def install_deployment_hooks(strategy, deployment, manifest_generator)
       install_datadog_hook(strategy, deployment.bosh_environment)
-
       install_token_hook(strategy, manifest_generator)
     end
 
     def promote_branch(strategy)
       return unless env.options.promote_branch
+
       strategy.promote_to!(env.options.promote_branch)
     end
 
     def install_token_hook(strategy, manifest_generator)
       return unless env.options.install_tokens
+
       strategy.install_hook TokenInstaller.new(manifest_generator, env.runner)
     end
 
     def install_datadog_hook(strategy, env)
-      if env["DATADOG_API_KEY"]
-        dogapi = Dogapi::Client.new(
-          env["DATADOG_API_KEY"],
-          env["DATADOG_APPLICATION_KEY"])
+      return unless env.has_key?("DATADOG_API_KEY")
 
-        strategy.install_hook(
-          DatadogEmitter.new(@logger, dogapi, @env.options.deployment_name))
-      end
+      dogapi = Dogapi::Client.new(env["DATADOG_API_KEY"], env["DATADOG_APPLICATION_KEY"])
+      strategy.install_hook(DatadogEmitter.new(@logger, dogapi, @env.options.deployment_name))
     end
   end
 end
