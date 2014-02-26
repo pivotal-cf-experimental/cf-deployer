953753097511736ef63343d2ad9982a4228a7f0e
Mark Rushakoff <mrushakoff@pivotallabs.com>
2014-02-26 14:28:21 -0800

    Prefer attrs over instance variables in DeployEnvironment
    Signed-off-by: Aram Price <aprice@pivotallabs.com>

--- lib/cf_deployer/deploy_environment.rb
+++ lib/cf_deployer/deploy_environment.rb
@@ -17,48 +17,48 @@ module CfDeployer
     end
 
     def prepare
-      deployments_repo = Repo.new(@logger, @runner, @options.repos_path, @options.deployments_repo, "origin/master")
-      deployments_repo.sync! unless @options.dirty
+      deployments_repo = Repo.new(@logger, @runner, options.repos_path, options.deployments_repo, "origin/master")
+      deployments_repo.sync! unless options.dirty
 
-      release_repo = ReleaseRepo.new(@logger, @runner, @options.repos_path, @options.release_repo, @options.release_ref)
-      release_repo.sync! unless @options.dirty
+      release_repo = ReleaseRepo.new(@logger, @runner, options.repos_path, options.release_repo, options.release_ref)
+      release_repo.sync! unless options.dirty
 
-      @deployment = Deployment.new(File.join(deployments_repo.path, @options.deployment_name))
+      @deployment = Deployment.new(File.join(deployments_repo.path, options.deployment_name))
 
       bosh = Bosh.new(
-        @logger, @runner, deployment.bosh_environment,
-        interactive: @options.interactive,
-        rebase: @options.rebase,
-        dirty: @options.dirty,
-        dry_run: @options.dry_run
+        @logger, runner, deployment.bosh_environment,
+        interactive: options.interactive,
+        rebase: options.rebase,
+        dirty: options.dirty,
+        dry_run: options.dry_run
       )
 
-      @manifest_generator = ReleaseManifestGenerator.new(@runner, release_repo, @options.infrastructure, "new_deployment.yml")
+      @manifest_generator = ReleaseManifestGenerator.new(@runner, release_repo, options.infrastructure, "new_deployment.yml")
 
-      build_deployment_strategy(deployment, bosh, manifest_generator, @options.release_name, release_repo)
+      build_deployment_strategy(bosh, release_repo)
     end
 
     private
 
-    def build_deployment_strategy(deployment, bosh, manifest_generator, release_name, release_repo)
+    def build_deployment_strategy(bosh, release_repo)
       strategy_type =
-        if @options.final_release
+        if options.final_release
           FinalDeploymentStrategy
         else
           DevDeploymentStrategy
         end
 
-      needs_explicit_director_uuid = @options.infrastructure == "warden"
+      needs_explicit_director_uuid = options.infrastructure == "warden"
       if needs_explicit_director_uuid
         manifest_generator.overrides["director_uuid"] = bosh.director_uuid
       end
 
-      if @options.manifest_domain
+      if options.manifest_domain
         manifest_generator.overrides["properties"] ||= {}
-        manifest_generator.overrides["properties"]["domain"] = @options.manifest_domain
+        manifest_generator.overrides["properties"]["domain"] = options.manifest_domain
       end
 
-      @strategy = strategy_type.new(bosh, deployment, manifest_generator, release_name, release_repo)
+      @strategy = strategy_type.new(bosh, deployment, manifest_generator, options.release_name, release_repo)
     end
   end
 end
